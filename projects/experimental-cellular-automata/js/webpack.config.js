8d8323b9310fcba03309bc7ad9f21af496d4e5e3
Samuel Monnier <samuel.monnier@pm.me>
2024-05-06 00:58:44 +0100

    Add neighborhood types, periodicity Had to disable mangling, fix that!

--- projects/experimental-cellular-automata/js/webpack.config.js
+++ projects/experimental-cellular-automata/js/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     minimizer: [new TerserPlugin({
       terserOptions: {
         mangle: {
-          properties: true,
+          properties: false,
         },
       },
     }),
