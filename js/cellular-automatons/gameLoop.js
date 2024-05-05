c6e0beecd89a5f55973a9d4887d088f812a58113
Samuel Monnier <samuel.monnier@pm.me>
2024-05-05 23:06:01 +0100

    More sensible global time periodicity

--- js/cellular-automatons/gameLoop.js
+++ js/cellular-automatons/gameLoop.js
@@ -5,11 +5,13 @@ import { updatePeriodicityShiftAndTopology } from './interactivity/optionSetter.
 import { drawGrid } from './draw/drawGrid.js';
 import { updateGrid } from './updateGrid.js';
 
+var globalPeriodicity = 2 * 2 * 2 * 2 * 3 * 3 * 3 * 5 * 7 * 11;
+
 export function gameLoop(globalData) {
     drawGrid(globalData);
     updateGrid(globalData);
     changeRule(globalData)
-    globalData.time = (globalData.time + 1) % 4;
+    globalData.time = (globalData.time + 1) % globalPeriodicity;
 
     updatePeriodicityShiftAndTopology(globalData);
     
