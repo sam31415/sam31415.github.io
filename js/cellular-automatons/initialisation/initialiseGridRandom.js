7c1bbba2525568be30bf7e7acb43313d4d5e7255
Samuel Monnier <samuel.monnier@pm.me>
2024-05-15 19:01:57 +0100

    Same with the random grid initialisation

--- js/cellular-automatons/initialisation/initialiseGridRandom.js
+++ js/cellular-automatons/initialisation/initialiseGridRandom.js
@@ -1,31 +1,6 @@
 import { Grid } from "../classes/grid.js";
 
-
-// Initialize the grid randomly
-
-// export function initialiseGridRandom(globalData) {
-//     return Promise.resolve().then(() => {
-//         globalData.grid = new Grid(globalData.gridWidth, globalData.gridHeight);
-//         globalData.redraw = new Grid(globalData.gridWidth, globalData.gridHeight);
-//         for (var i = 0; i < globalData.gridHeight; i++) {
-//             for (var j = 0; j < globalData.gridWidth; j++) {
-//                 if (globalData.initialisation == "gr" && globalData.mask.get(i, j) === 1) {
-//                     globalData.grid.set(i, j, 0);
-//                     continue;
-//                 }
-//                 var rnd = Math.random();
-//                 globalData.grid.set(i, j, 0);
-//                 globalData.grid.set(i, j, rnd < 0.75 ? 1 : globalData.grid.get(i, j));
-//                 globalData.grid.set(i, j, rnd < 0.5 ? 2 : globalData.grid.get(i, j));
-//                 globalData.grid.set(i, j, rnd < 0.25 ? 3 : globalData.grid.get(i, j));
-
-//                 globalData.redraw.set(i, j, 1);
-//             }
-//         }
-//     });
-// }
-
-export function initialiseGridRandom(globalData, n = 64) {
+export function initialiseGridRandom(globalData, n = 10000) {
     return Promise.resolve().then(() => {
         globalData.grid = new Grid(globalData.gridWidth, globalData.gridHeight);
         globalData.redraw = new Grid(globalData.gridWidth, globalData.gridHeight);
