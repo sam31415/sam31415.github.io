8d91f9032c1ae64f18179319aa829b8f71bb3d1c
Samuel Monnier <samuel.monnier@pm.me>
2024-05-15 22:34:23 +0100

    Update webpack config for mouse demo

--- projects/gr-game/js/webpack.config.js
+++ projects/gr-game/js/webpack.config.js
@@ -1,22 +1,42 @@
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './main.js',
-  output: {
-    filename: 'output.js',
-    path: __dirname
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin({
-      terserOptions: {
-        mangle: {
-          properties: true,
+module.exports = [
+  {
+    entry: './main.js',
+    output: {
+      filename: 'output.js',
+      path: __dirname
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          mangle: {
+            properties: true,
+          },
         },
-      },
-    }),
-  ],
+      }),
+    ],
+    },
   },
-};
+  {
+    entry: './mainMouse.js',
+    output: {
+      filename: 'outputMouse.js',
+      path: __dirname
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          mangle: {
+            properties: true,
+          },
+        },
+      }),
+    ],
+    },
+  }
+];
 
 // Run npx webpack in the folder to build the project
