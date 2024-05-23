dd17524e4a9c357b6bde986e913db3102962deef
Samuel Monnier <samuel.monnier@pm.me>
2024-05-24 00:00:27 +0100

    Save rule as text (unused for now)

--- js/cellular-automatons/interactivity/saveEventData.js
+++ js/cellular-automatons/interactivity/saveEventData.js
@@ -3,20 +3,28 @@ export function saveEventData(globalData, event) {
         return;
     }
     // Extract the rule definition and button information
-    var ruleDefinition = {...globalData.ruleDefinition};
-    delete ruleDefinition.secondaryRule;  // Exclude the secondaryRule entry
-    delete ruleDefinition.conditions;  // Exclude the conditions entry
-    var data = {...ruleDefinition, event: event};
+    // var ruleDefinition = {...globalData.ruleDefinition};
+    // delete ruleDefinition.secondaryRule;  // Exclude the secondaryRule entry
+    // delete ruleDefinition.conditions;  // Exclude the conditions entry
+    // var data = {...ruleDefinition, event: event};
 
-    // Convert the data to CSV
-    var csv = Object.keys(data).join(',') + '\n';  // Add the column names
-    csv += Object.values(data).join(',');  // Add the data
+    // // Convert the data to CSV
+    // var csv = Object.keys(data).join(',') + '\n';  // Add the column names
+    // csv += Object.values(data).join(',');  // Add the data
 
-    // Save the CSV to a file
-    var blob = new Blob([csv], {type: 'text/csv'});
+    // // Save the CSV to a file
+    // var blob = new Blob([csv], {type: 'text/csv'});
+    // var url = URL.createObjectURL(blob);
+    // var link = document.createElement('a');
+    // link.href = url;
+    // link.download = 'dataAutomata.csv';
+    // link.click();
+
+    var data = globalData.ruleClass.ruleChain[0].getName();
+    var blob = new Blob([data], {type: 'text/plain'});
     var url = URL.createObjectURL(blob);
     var link = document.createElement('a');
     link.href = url;
-    link.download = 'dataAutomata.csv';
+    link.download = 'dataAutomata.txt';
     link.click();
 }
